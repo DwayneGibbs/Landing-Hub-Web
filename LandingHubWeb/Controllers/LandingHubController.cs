1cd1ef95166f49d243cd095057fe0dece4af5131
Dwayne Gibbs <DwayneGibbs2000@hotmail.com>
2019-05-31 06:27:14 -0500

    Connection to Gravatar and loaded reference icons and logos

---
+++ LandingHubWeb/Controllers/LandingHubController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LandingHubWeb.Controllers
+{
+    public class LandingHubController : Controller
+    {
+        // GET: LandingHub
+        public ActionResult Index()
+        {
+            string GravatarEmail = System.Configuration.ConfigurationManager.AppSettings["GravatarEmailAddress"].ToString();
+
+            Services.StringToMD5 stringToMD5 = new Services.StringToMD5();
+            ViewBag.GravatarHashedEmail = "https://www.gravatar.com/avatar/" + stringToMD5.CreateMD5(GravatarEmail) + "?s=200.jpg";
+
+            return View();
+        }
+    }
+}
